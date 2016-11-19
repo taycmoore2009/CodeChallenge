68bef75083fc16ab947f19d7779ed070fdea4036
Tay Moore <taycmoore2009@gmail.com>
2016-11-19 13:39:13 -0600

    CC: Finished building issue viewer

--- scripts/issueView.js
+++ scripts/issueView.js
@@ -1,6 +1,11 @@
 (function(){
     var IssueView = Backbone.View.extend({
         events: {
+            "click [data-link]": "openLink"
+        },
+
+        openLink: function(e){
+            window.open($(e.currentTarget).data("link"), "_blank");
         },
 
         /* render issue on the screen */
@@ -11,30 +16,31 @@
 
                 /* render HTML */
                 /* -- Needs to be changed to use _.templates -- */
+                $("h1").html("Issue: #"+ issue.number +"<br/><span>"+ issue.title +"</span>");
                 this.$el.html(function(){
                     /* create html */
                     var html = "";
-                    html += "<div class='issueItem' data-link='"+ issue.number +"'>"+
-                                "<div class='issueItemUser'><img src='"+ issue.user.avatar_url +"' /><span>"+ issue.user.login +"</span></div>"+
+                    
+                    html += "<div class='issueItem'>"+
+                                "<div class='issueItemUser' data-link='"+ issue.user.html_url +"'><img src='"+ issue.user.avatar_url +"' /><span>@"+ issue.user.login +"</span></div>"+
                                 "<div class='issueItemContent'>"+
-                                    "<div class='issueItemTitle'>"+ issue.title +"<span>#"+ issue.number +"</span></div>"+
                                     "<div class='issueItemLabels'>";
                     if(issue.labels.length > 0){
                         $(issue.labels).each(function(){
-                            html += "<span style='background-color: #"+ issue.color +"'>"+ issue.name +"</span>";
+                            html += "<span style='background-color: #"+ this.color +"'>"+ this.name +"</span>";
                         });
                     } else {
                         html += "<span class='noLabels'>No Labels</span>";
                     }
                     html +=         "</div>"+    
-                                    "<div class='issueItemShortDesc'>"+ issue.body +"</div>"+
-                                    "<div class='comments'></div>"
+                                    "<div class='issueItemDesc'>"+ issue.body +"</div>"+
                                 "</div>"+
-                            "</div>";
+                            "</div>"+
+                            "<div class='comments'></div>";
                     return html;
                 });
                 if(issue.comments > 0){
-                    this.getComments(issues.comments_url);
+                    this.getComments(issue.comments_url);
                 }
             }.bind(this));
         },
@@ -44,8 +50,21 @@
             $.get(comments_url).then(function(comments){
                 this.$el.find(".comments").html(function(){
                     var html = "";
-                    $(comments).each(function(comment){
-                        /*---- Add comment stuff here ----*/
+                    $(comments).each(function(){
+                        html += "<div class='comment'>" +
+                                    "<div class='commentUser' data-link='"+ this.user.html_url +"'>"+
+                                        "<img src='"+ this.user.avatar_url +"' />"+
+                                        "<span>@"+ this.user.login +"</span>"+
+                                    "</div>"+ 
+                                    "<div class='commentBody'>"+
+                                        "<div class='commentBodyHeader'>"+
+                                            "<span>Comment #"+ this.id +" </span>"+
+                                            "<span>Created "+ this.created_at +" | </span>"+
+                                            "<span>Updated "+ this.updated_at +"</span>"+
+                                        "</div>"+
+                                        "<div class='commentBodyContent'>"+ this.body +"</div>"+
+                                    "</div>"+
+                                "</div>"
                     });
                     return html;
                 });
